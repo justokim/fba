b24ea88bb37e2941948381571332e0a9d53f4e59
justin kim <72166250+justokim@users.noreply.github.com>
2024-11-21 20:16:09 -0800

    free class implement

---
+++ app/components/Index/FreeClass.js
@@ -0,0 +1,9 @@
+export const FreeClass = () => {
+    return(
+        <div className="py-8">
+        <h1 className="lg:text-6xl text-3xl font-bold text-center py-8 text-[#6D31ED]">
+          First Class is Free!
+        </h1>
+    
+      </div>    )
+}
